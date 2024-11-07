4aadda6ec460c303b81a6f5d42130ba91a796cae
Ajitha Narraa <ajithanarra23@gmail.com>
2024-11-06 20:20:54 -0600

    updates artifacts with bonus

--- routes/grid.js
+++ routes/grid.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET grid page. */
+/* GET grid page with query parameters */
 router.get('/', function(req, res, next) {
   let query = req.query;
   console.log(`rows ${query.rows}`);
